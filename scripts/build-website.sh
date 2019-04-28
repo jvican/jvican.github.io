d070745fe13f012a2b31d4f7e3227e3297f3557d
Jorge Vicente Cantero <jorgevc@fastmail.es>
2019-04-28 22:57:22 +0200

    Update style

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -1,4 +1,4 @@
-pandoc -s index.md -o index.html --metadata pagetitle="@jvican's blog"
-pandoc -s posts/overload-methods-with-more-parameter-lists.md -o posts/overload-methods-with-more-parameter-lists.html --metadata pagetitle="Overload methods with more parameters lists"
-pandoc -s posts/scalac-profiling.md -o posts/scalac-profiling.html --metadata pagetitle="Profiling and reducing compilation times"
-pandoc -s posts/git-sbt-analysis.md -o posts/git-sbt-analysis.html --metadata pagetitle="How often do we change our sbt builds?"
+pandoc index.md -o index.html --metadata pagetitle="@jvican's blog"
+pandoc posts/overload-methods-with-more-parameter-lists.md -o posts/overload-methods-with-more-parameter-lists.html --metadata pagetitle="Overload methods with more parameters lists"
+pandoc posts/scalac-profiling.md -o posts/scalac-profiling.html --metadata pagetitle="Profiling and reducing compilation times"
+pandoc posts/git-sbt-analysis.md -o posts/git-sbt-analysis.html --metadata pagetitle="How often do we change our sbt builds?"
